85eabbc9336af2532a9ccdcad158ac257403d34e
s19010 <s19010@std.it-college.ac.jp>
2020-01-15 16:13:17 +0900

    ストップウォッチ

--- src/App.js
+++ src/App.js
@@ -1,17 +1,75 @@
 import React from 'react'
 
-const Test = props => {
-  const comment = 'Hello'
-  return <h1>{comment}</h1>
-}
-
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      isLive: false,
+      curTime: 0,
+      startTIme: 0
+    }
+    this.timerId = 0
+  }
+
+  componentWillMount () {
+    this.timerId = setInterval(e => {
+      this.tick()
+    }, 1000)
+  }
+
+  componentWillUnmount () {
+      clearInterval(this.timerId)
+  }
+  
+
+  tick () {
+    if (this.state.isLive) {
+      const v = new Date().getTime()
+      this.setState({ curTime: v })
+    }
+  }
+
+  clickHndler (e) {
+    if (this.state.isLive) {
+      this.setState({ isLive: false })
+      return
+    }
+  }
+
+    const v = new Date().getTime()
+    this.setState({
+        curTime: v,
+        startTime: v,
+        isLive: true})
+
+  getDisp () {
+      const s = this.state
+      const delta = s.curTIme - s.startTime
+      const t = Math.floor(delta / 1000)
+      const ss = t % 60
+      const m = Math.foor(t / 60)
+      const mm = m % 60
+      const hh = Math.floor(mm / 60)
+      const z = (num) => {
+          const s = '00' + String(num)
+          return s.substr(s.length - 2, 2)
+      }
+      return <span className='disp'>
+          {z(hh)}:{z(mm)}:{z(ss)}
+      </span>
   }
   render () {
-    return <Test />
+    let label = 'START'
+    if (this.state.isLive) {
+        label = 'STOP'
+    }
+    const disp = this.getDisp()
+    const fclick = (e) => this.clickHndler(e)
+    return (<div className='Stopwatch'>
+      <div>(disp</div>
+      <button onClick={fclick}>{label}</button>
+    </div>)
   }
 }
+
 export default App
