771e7b82757aaa04fdd32f39e8d939e304f95e0c
s19010 <s19010@std.it-college.ac.jp>
2020-01-15 19:11:43 +0900

    フォーム作成

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import React from 'react'
 
-const Test = props => {
-  const comment = 'Hello'
-  return <h1>{comment}</h1>
+/*const TestView = props => {
+  <div>{props.value}</div>
+}
+*/
+
+class TestForm extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { value: '' }
+    this.doSubmit = this.doSubmit.bind(this)
+    this.doChange = this.doChange.bind(this)
+  }
+
+  doChange (e) {
+    this.setState({ value: e.target.value })
+  }
+
+  doSubmit (e) {
+    e.preventDefault()
+    alert('値を送信: ' + this.state.value)
+  }
+
+  render () {
+    return (
+      <form action='' onSubmit={this.doSubmit}>
+        <input type='text' onChange={this.doChange} />
+        <input type='submit' value='送信' />
+      </form>
+    )
+  }
 }
 
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = { value: '' }
   }
+
   render () {
-    return <Test />
+    return (
+      <div>
+        {/*<TestView value={this.state.value} />*/}
+        <TestForm />
+      </div>
+    )
   }
 }
+
 export default App
