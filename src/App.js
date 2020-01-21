2571c69f9b3475d220c2ffa4e24dbb365bc384ba
s19010 <s19010@std.it-college.ac.jp>
2020-01-21 16:38:32 +0900

    色選択コンポーネント

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react'
 
-const Greeting = props => <li>{props.type}</li>
+class ColorBox extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      index: 0,
+      colors: ['red', 'blue', 'green', 'yellow', 'black']
+    }
+  }
+
+  render () {
+    const colors = this.state.colors
+    const index = this.state.index
+    const cur = index >= 0 ? colors[index] : ''
+    const items = colors.map(c => {
+      let bs = '1px solid white'
+      if (c === cur) {
+        bs = '1px solid black'
+      }
+      const cstyle = {
+        color: c,
+        border: bs
+      }
+      const clickHandler = e => {
+        const c = e.target.style.color
+        const i = this.state.colors.indexOf(c)
+        this.setState((index: i))
+        if (this.props.onColorChange) {
+          this.props.onColorChange({ color: c })
+        }
+      }
+      return (
+        <span onClick={clickHandler} style={cstyle}>
+          ■
+        </span>
+      )
+    })
+    return <div>{items}</div>
+  }
+}
 
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = { data: [] }
+    this.state = { value: '' }
   }
 
   render () {
-    const greetings = this.state.data.map((v, i) => (
-      <Greeting type={v} key={i} />
-    ))
-
-    return <ul>{greetings}</ul>
-  }
-
-  componentDidMount () {
-    fetch('./messages.json')
-      .then(res => res.json())
-      .then(json => json.messages)
-      .then(json => this.setState({ data: json }))
+    return (
+      <div>
+        <ColorBox />
+      </div>
+    )
   }
 }
 
